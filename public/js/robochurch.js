a251ab3843f4120826f559d0d5fbabc646938185
Nathan Livni <nathanlivni@gmail.com>
2015-08-26 11:57:45 -0500

    side light animation created

--- public/js/robochurch.js
+++ public/js/robochurch.js
@@ -8,6 +8,7 @@ $(function(){
 
 var _Margin = 20;
 
+// jquery variables
 var $windowWidth = $(window).width();
 var $windowHeight = $(window).height();
 var $serverLeft = $('#server-left');
@@ -20,9 +21,18 @@ var $content = $('#mainframe-content ');
 var $serverSectionTop = $('.server-section-top');
 var $serverSectionMiddle = $('.server-section-middle');
 var $serverSectionBottom = $('.server-section-bottom');
+var $contentSectionBottom = $('#mainframe-content-bottom-container')
 var $serverVentTop = $('.server-vent-top');
 var $serverVentMiddle = $('.server-vent-middle');
 var $serverVentBottom = $('.server-vent-bottom');
+var $SeverVentSide = $('.server-vent-side');
+var $ServerMonitor = $('.server-monitor');
+
+// animation settings
+var _SideLightsStatus = 'inactive';
+var _ActiveSideLight = 0;
+var _SideLightDelta = 500;
+
 
 /*  so window resize is only called once per resize */
  var rtime;
@@ -81,7 +91,7 @@ function topVentHTML(){
         //console.log('currentRowID: ' + currentRowID);
         outputHTML += '<div id="'+ currentRowID +'" class=" row  '+ currentRowClass +'">';
         for(var j=0; j < numVentHoles; j++) {
-            outputHTML += '<div class="vent-hole col-sm-4" id="vent-hole-' + i + '-'+ j + '"></div>';
+            outputHTML += '<div class="vent-hole col-sm-4 pixelated" id="vent-hole-' + i + '-'+ j + '"></div>';
         }
         outputHTML += '</div>';
     }
@@ -90,16 +100,41 @@ function topVentHTML(){
     //$('.vent-hole').height($serverSectionTop.height() / (numVentRows + 1.2));
 }
 
+// TODO: draw graphics overlay for buttons
+// TODO: add graphics overlay to buttons in css
+// TODO: animate monitors using css background on timer
+
 function middleVentHTML(){
-    var numButtons = Math.floor(($('.server-vent-side').height() - 10) / ($('.server-vent-side').width() + 20));
+    var numButtons = Math.floor(($SeverVentSide.height() - 10) / ($SeverVentSide.width() + 20));
     console.log(numButtons);
     var outputHTML = '';
     for(var i=0; i < numButtons; i++) {
-        outputHTML += '<div id="side-button-' + i + '" class="side-button"></div>';
+        var sideLightClass = 'side-light side-light-' + i;
+        outputHTML += '<div id="side-light-' + i + '" class="'+ sideLightClass +'"></div>';
     }
-    $('.server-vent-side').html(outputHTML);
-    $('.side-button').height($('.side-button').width());
-    $('.server-monitor').height($('.server-monitor-container').height()/3);
+    $SeverVentSide.html(outputHTML);
+    //todo: problem with height spacing. should instead go:
+    // 1. width = 100%
+    // 2. height = width
+    // 3. margin top / bottom = ($('.server-monitor-container').height() - (height * 3) / 2)
+    $ServerMonitor.height($('.server-monitor-container').height()/3);
+    $ServerMonitor.width("");
+    if ($ServerMonitor.height() > $ServerMonitor.width()) {
+        $ServerMonitor.height($ServerMonitor.width());
+    } else {
+        $ServerMonitor.width($ServerMonitor.height());
+    }
+    //$ServerMonitor.height("");
+    var monitorMargin = (($('.server-monitor-container').height() - ($('.server-monitor').height() * 3)) / 2) + 'px';
+    $ServerMonitor.css('margin-top', monitorMargin);
+
+    $('.side-light').height($('.side-light').width());
+//$ServerMonitor.width()
+}
+
+// TODO: render bottom vent html
+function bottomVentHTML(){
+
 }
 
 function renderRobochurchCSS() {
@@ -110,14 +145,16 @@ function renderRobochurchCSS() {
     $windowHeight = $(window).height();
 
     $mainframeContainer.height($windowHeight);
-    $content.height($windowHeight - _Margin);
+    $content.height(($windowHeight *.75) - _Margin);
     $servers.height($windowHeight);
     $serverSectionTop.height($windowHeight * .25);
     $serverSectionMiddle.height($windowHeight * .50);
     $serverSectionBottom.height($windowHeight * .25);
+    $contentSectionBottom.height($windowHeight *.25);
     topVentHTML();
     middleVentHTML();
-    $serverSectionBottom.html('<p>serverSectionBottom</p>');
+    _SideLightsStatus = 'active';
+    activateSideLights();
 }
 
 function renderRobochurchCSSMobile() {
@@ -126,6 +163,34 @@ function renderRobochurchCSSMobile() {
     $serverLeft.addClass('hidden-xs');
     $serverRight.addClass('hidden-xs');
 
+}
+
+function activateSideLights(){
+    setTimeout(sideLightCycle, _SideLightDelta);
+}
+
+// have the lights transition from top to bottom and repeat
+function sideLightCycle() {
+    var numSideLights = $('.side-light').length/4 - 1;
+    if (_SideLightsStatus == 'inactive') {
+        // reset lights
+        $('.side-light').removeClass('active');
+        _ActiveSideLight = 0;
+        console.log('light status: ' + _SideLightsStatus)
+    } else {
+        console.log('light status: ' + _SideLightsStatus);
+        var newActiveSideLight = _ActiveSideLight + 1;
+        // shortcut because will always restart at 0
+        if (_ActiveSideLight >= numSideLights) {
+            newActiveSideLight = 0;
+        }
+        console.log(newActiveSideLight);
+        // cycle active light
+        //$('.side-light-'+_ActiveSideLight).removeClass('active');
+        $('.side-light-'+ newActiveSideLight).addClass('active');
+        _ActiveSideLight = newActiveSideLight;
+        setTimeout(sideLightCycle, _SideLightDelta);
+    }
 }
 
 
